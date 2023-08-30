8b74670ff2e85faa97fa55c71c0770b2cc7c8615
Jeter Trajano <jetertrajano2018@gmail.com>
2023-08-30 18:28:14 +0800

    Update files and add JS file fundamentals + activity

---
+++ 15-javascript_fundamentals/scripts.js
@@ -0,0 +1,17 @@
+console.log(23 + 97);
+
+console.log(2 + 5 + 90 + 22 + 9 + 77);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+a = undefined;
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = 57 - 13;
+let percentage = actual / max;
+console.log(percentage);
